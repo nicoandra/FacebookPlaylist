87b2ae611328c77baabae7c7c7bcde2a7a9d55a4
Nicolás Andrade <github@nmac.com.ar>
2014-12-31 00:56:37 -0500

    Adding config file Adding datastorage

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+// Facebook Settings
+define('FB_APP_ID','93917592302');
+define('FB_SECRET_KEY', '2d0f42e7d1a228fce41605309c80a2fb');
+define('FB_EVENT_ID', '1529633110618292');
+
+
+
+// Local settings
+define('MP3_FOLDER','/tmp/party');
+define('YOUTUBEDL_PATH','/usr/bin/youtube-dl');
+define('TIME_BETWEEN_RUNS', 600000000); // How often does this script run? (in seconds)
+define('COMMAND_TO_APPEND_A_SONG', '/usr/bin/clementine -a ');
+
+
+
---
+++ datastorage.php
@@ -0,0 +1,32 @@
+<?php
+
+class DataStorage {
+	
+	private static $redis = false;
+
+	public static function init(){
+		self::$redis = new Redis();
+		self::$redis->connect('127.0.0.1');
+	}
+
+	public static function setLastVerificationTimestamp($time = NULL){
+		if(is_null($time) || (int) $time !== $time){
+			$time = mktime();
+		}
+
+		self::$redis->hSet('partyInfo','lastVerificationTimestamp', $time);
+	}
+
+	public static function getLastVerificationTimestamp(){
+		return (int) self::$redis->hGet('partyInfo', 'lastVerificationTimestamp');
+	}
+
+	public static function markStatusAsVerified($status){
+		return self::$redis->sAdd('partyInfo:processedEntries', $status);
+	}
+
+	public static function isStatusVerified($status){
+		return self::$redis->sIsMember('partyInfo:processedEntries', $status);
+	}
+
+}
